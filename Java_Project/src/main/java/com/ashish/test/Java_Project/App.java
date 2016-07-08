545377bb18842cce32beade2e4858d4491af655a
ashi bansal <bansal.ashish.obra@gmail.com>
2016-07-08 21:46:08 +0530

    Test Maven Project

---
+++ Java_Project/src/main/java/com/ashish/test/Java_Project/App.java
@@ -0,0 +1,18 @@
+package com.ashish.test.Java_Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public int sum(int no1, int no2){
+		
+		return no1+no2;
+	}
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
